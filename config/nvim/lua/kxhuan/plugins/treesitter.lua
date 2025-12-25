708e9c1ba808547f9456f7c40ae01aa4def2ef9f
Kun-Yao Huang <khuang@anduril.com>
2025-12-25 22:00:36 +1100

    fix treesitter

--- config/nvim/lua/kxhuan/plugins/treesitter.lua
+++ config/nvim/lua/kxhuan/plugins/treesitter.lua
@@ -1,7 +1,30 @@
 -- Treesitter is provided by Nix (home-manager) with pre-compiled parsers
--- This avoids compilation issues on NixOS
--- Configuration is in modules/neovim.nix extraLuaConfig
+-- lazy.nvim will use the Nix-provided version and just run this config
 return {
   "nvim-treesitter/nvim-treesitter",
-  enabled = false,  -- Skip lazy.nvim management, use Nix version
+  lazy = false,  -- Load immediately (before other plugins need it)
+  priority = 100,  -- Load early but after colorscheme
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    -- Configure the Nix-provided treesitter plugin
+    require('nvim-treesitter.configs').setup({
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      autotag = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
 }
--- modules/neovim.nix
+++ modules/neovim.nix
@@ -8,9 +8,10 @@
         vimAlias = true;
 
         # Provide problematic plugins via Nix (pre-compiled)
-        # lazy.nvim will manage the rest
+        # lazy.nvim will recognize and configure them (see config/nvim/lua/kxhuan/plugins/)
         plugins = with pkgs.vimPlugins; [
             # Treesitter with pre-compiled parsers (avoids compilation issues)
+            # Configuration is in config/nvim/lua/kxhuan/plugins/treesitter.lua
             (nvim-treesitter.withPlugins (p: [
                 p.c
                 p.cpp
@@ -35,25 +36,6 @@
             tree-sitter
             nodejs
         ];
-
-        # Configure treesitter (since it's not managed by lazy.nvim)
-        extraLuaConfig = ''
-          -- Treesitter configuration (Nix-provided plugin)
-          require('nvim-treesitter.configs').setup({
-            highlight = { enable = true },
-            indent = { enable = true },
-            autotag = { enable = true },
-            incremental_selection = {
-              enable = true,
-              keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-              },
-            },
-          })
-        '';
     };
 
     # Link individual neovim config files/directories
